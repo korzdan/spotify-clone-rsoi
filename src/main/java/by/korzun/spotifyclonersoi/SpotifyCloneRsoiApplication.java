1a74aca472bb3f8c2d2d9c08ed58909c49e0604e
Filina-vn <filina26112002@gmail.com>
2023-02-07 19:50:40 +0300

    feat: initial commit

---
+++ src/main/java/by/korzun/spotifyclonersoi/SpotifyCloneRsoiApplication.java
@@ -0,0 +1,13 @@
+package by.korzun.spotifyclonersoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyCloneRsoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyCloneRsoiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/korzun/spotifyclonersoi/SpotifyCloneRsoiApplicationTests.java
@@ -0,0 +1,13 @@
+package by.korzun.spotifyclonersoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyCloneRsoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

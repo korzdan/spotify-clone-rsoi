bad005cd34c9a172b80bb3431018941ec1556ac6
korzdan <korzundanik@gmail.com>
2023-02-07 23:28:51 +0300

    refactor: refactor TrackHistoryService

--- src/main/java/by/korzun/spotify_clone_rsoi/service/TrackHistoryService.java
+++ src/main/java/by/korzun/spotify_clone_rsoi/service/TrackHistoryService.java
@@ -2,11 +2,14 @@ package by.korzun.spotify_clone_rsoi.service;
 
 import by.korzun.spotify_clone_rsoi.domain.Playlist;
 import by.korzun.spotify_clone_rsoi.domain.TrackHistory;
+import by.korzun.spotify_clone_rsoi.util.DateUtils;
 import lombok.AllArgsConstructor;
 
-import java.time.LocalDateTime;
 import java.time.YearMonth;
-import java.util.*;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @AllArgsConstructor
@@ -16,7 +19,7 @@ public class TrackHistoryService {
 
     private static final Integer NUMBER_OF_TOP_TRACKS = 3;
 
-    // TODO: Test this method
+    // TODO: Refactor this method. Returns unsorted tracks.
     public Playlist getPlaylistOfTopTracksOfTheMonth(YearMonth month) {
         Playlist playlist = new Playlist(new HashSet<>());
         allTrackHistory.stream()
@@ -32,12 +35,7 @@ public class TrackHistoryService {
 
     private Long countTrackPlaysInMonth(TrackHistory trackHistory ,YearMonth month) {
         return trackHistory.getPlayDates().stream()
-                .filter(date -> isDateInMonth(date, month))
+                .filter(date -> DateUtils.isDateInMonth(date, month))
                 .count();
-    }
-
-    private boolean isDateInMonth(LocalDateTime date, YearMonth month) {
-        return date.isAfter(month.atDay(1).atStartOfDay()) &&
-                date.isBefore(month.plusMonths(1).atDay(1).atStartOfDay());
     }
 }

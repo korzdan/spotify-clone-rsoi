d40e126111abb3582a2bfd6ab7caa234c45c94e5
korzdan <korzundanik@gmail.com>
2023-02-12 00:35:11 +0300

    feat: init new Spring module and fix build.gradle of spotify-spring-console

---
+++ spotify-spring/src/main/java/by/korzun/spotifyspring/SpotifySpringApplication.java
@@ -0,0 +1,13 @@
+package by.korzun.spotifyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifySpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifySpringApplication.class, args);
+    }
+
+}
